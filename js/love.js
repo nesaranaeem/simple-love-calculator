7302215872c1e0b7f6e33336840e52c7d8b818ce
nesaranaeem <hello@nesaran.com>
2022-08-16 17:35:28 +0600

    simple comment added

--- js/love.js
+++ js/love.js
@@ -3,6 +3,7 @@ document
   .addEventListener("click", function () {
     const inputField = document.getElementById("lover-name-input");
     const loverName = inputField.value;
+
     const showLoverName = document.getElementById("lover-name");
     showLoverName.innerText = loverName.toUpperCase();
     //jantus name
@@ -12,11 +13,29 @@ document
     showJantusName.innerText = jantusName.toUpperCase();
     //result
     const result = document.getElementById("calculation-result");
-    let finalResult = Math.floor(Math.random() * 101) + "%";
-    result.innerText = finalResult;
-
+    let finalResult = Math.floor(Math.random() * 101);
+    let theFinalResult = parseInt(finalResult);
+    result.innerText = theFinalResult + "%";
+    //comment
+    let gururComment = document.getElementById("gurur-comment");
+    if (theFinalResult < 40) {
+      gururComment.innerText =
+        "খুবি খারাপ অবস্থা! দেশে সব কিছুর দাম বেশি, তাই ভালোবাসার ও বাজারদর খারাপ! চিন্তার কারণ নেই!!";
+    } else if (theFinalResult > 40 && theFinalResult < 60) {
+      gururComment.innerText =
+        "খারাপ না! তবে আরেকটু বেশি ভালোবাসতে হবে! সমস্যা নেই";
+    } else if (theFinalResult > 60 && theFinalResult < 80) {
+      gururComment.innerText = "ব্যাপক! ২০২২ সালের লাইলি মজনু! শুভ কামনা";
+    } else {
+      gururComment.innerText = "সেরা! ২০২২ এর লেটেষ্ট লাইলি মজনু";
+    }
     //validation
     if (loverName == "" && jantusName == "") {
-      result.innerText = "0%, Coz, You don't enter your or your jantus name";
+      alert("সমস্যা! নাম সঠিক ভাবে দিতে হবে!");
+      window.location.href = "index.html";
     }
+
+    //clear
+    inputField.value = "";
+    inputField2.value = "";
   });
